96be4e19132a06af96a989a257ace990675ab7f6
drag-olya <>
2025-11-12 09:18:23 +0000

    Introduction to if Statement

---
+++ python_if_statement/introduction_to_if_statement/main.py
@@ -0,0 +1,6 @@
+string = 'codefinity'
+# Calculating the length of the string
+len_of_string = ___
+# Creating `if` statement
+if ___
+    print('Codefinity is the best platform to start your career in IT.')
